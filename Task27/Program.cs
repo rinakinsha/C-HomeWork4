5586e7eafb09e5d80984cc16bcdb3f9f318c61cf
iryna <rinakinsha@yndex.by>
2022-07-03 20:50:37 +0300

    Solution of task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿int SumOfDigits(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        int remainder = num % 10;
+        num = num / 10;
+        sum = sum + remainder;
+    }
+    return sum;
+}
+
+Console.Write("Пожалуйста, введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumOfDigits(number);
+
+Console.WriteLine("Сумма цифр введенного числа: " + result);
