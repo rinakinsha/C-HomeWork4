195081b4d32dcdaa26e059ce7112d421e4af3d8d
iryna <rinakinsha@yndex.by>
2022-07-04 00:11:57 +0300

    Solution of task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿void RandomArray(int elements)
+{
+    int[] array = new int[elements];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.Write("Введите желаемое количество элементов массива: ");
+int elem = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Сгенерированный массив: ");
+
+RandomArray(elem);
